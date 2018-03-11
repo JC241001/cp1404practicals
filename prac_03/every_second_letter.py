540d27d8177aaa27b0218a0662e95078f5f0e6f0
jc241001 <gerard.djian@my.jcu.edu.au>
2018-03-11 15:44:07 +1000

    Refactor with functions and input validation

--- prac_03/every_second_letter.py
+++ prac_03/every_second_letter.py
@@ -2,8 +2,37 @@
 Prints every second letter of provided name"""
 
 
-name = input("Enter name: ")
-while name == "":
-    print("Must enter something")
+def main():
+    name = get_name()
+    letter_freq = get_letter_freq()
+    print_every_nth_letter(name, letter_freq)
+
+
+def print_every_nth_letter(name, n):
+    print("Every 2nd letter is: {}".format(name[::n]))
+
+
+def get_name():
     name = input("Enter name: ")
-print("Every 2nd letter is: {}".format(name[::2]))
+    while name == "":
+        print("Must enter something")
+        name = input("Enter name: ")
+    return name
+
+
+def get_letter_freq():
+    valid_integer = False
+    while not valid_integer:
+        try:
+            freq = int(input("Enter letter frequency: "))
+            if freq < 1:
+                print("Please enter positive integer")
+                continue
+            valid_integer = True
+        except ValueError:
+            print("Invalid (not an integer)")
+    return freq
+
+
+if __name__ == '__main__':
+    main()
