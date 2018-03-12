9380d4f5709c3eb071907fe16a68cc61f5897a4c
jc241001 <gerard.djian@my.jcu.edu.au>
2018-03-12 12:25:16 +1000

    refactor methods and add validity checking

--- prac_03/Extension/word_gen.py
+++ prac_03/Extension/word_gen.py
@@ -11,13 +11,40 @@ VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
 
-word_format = input("Enter your word format with c for consonants and v for vowels: ")
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
+def main():
+    print("This is a random word generator.")
+    input_string = input("Enter your word format with c for consonants and v for vowels: ")
+    while not is_valid_format(input_string):
+        print("Your input is not valid, only enter lowercase c or lowercase v as a sequence")
+        input_string = input("Enter your word format with c for consonants and v for vowels: ")
+
+    random_word = generate_random_word(input_string)
+    print(random_word)
+
+
+def is_valid_format(a_string):
+    """check if a_string is only a sequence of c's and v's"""
+    valid_letters = ['c', 'v']
+    if a_string == "":
+        return False
     else:
-        word += random.choice(VOWELS)
+        for char in a_string:
+            if char not in valid_letters:
+                return False
+    return True
 
 
-print(word)
+def generate_random_word(cv_string):
+    """Only provide sequence of c's and v's.  Returns random word"""
+    word = ""
+    for speech_sound in cv_string:
+        if speech_sound == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+
+    return word
+
+
+if __name__ == '__main__':
+    main()
