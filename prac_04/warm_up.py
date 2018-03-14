a8cf6322ae62ea57538dd5b144c343bdfe081082
jc241001 <gerard.djian@my.jcu.edu.au>
2018-03-14 16:27:13 +1000

    add finger exercises

---
+++ prac_04/warm_up.py
@@ -0,0 +1,22 @@
+"""Warm up exercies"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+#In IDLE
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+print()
+
+#Expressions
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers)
+print(numbers[2:])
+print(9 in numbers)
