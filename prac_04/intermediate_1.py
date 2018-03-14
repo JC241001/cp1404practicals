9c2528a27345965eb809ce3cad0576d3674b238a
jc241001 <gerard.djian@my.jcu.edu.au>
2018-03-14 17:00:26 +1000

    simple program build

--- prac_04/intermediate_1.py
+++ prac_04/intermediate_1.py
@@ -1,1 +1,28 @@
+"""Intermediate exercise with lists of numbers"""
 
+
+def main():
+    numbers = []
+    for i in range(5):
+        numbers.append(get_number())
+
+    print("The first number is: {}".format(numbers[0]))
+    print("The last number is: {}".format(numbers[-1]))
+    print("The smallest number is: {}".format(min(numbers)))
+    print("The largest number is: {}".format(max(numbers)))
+    print("The average of the numbers is: {}".format(sum(numbers)/len(numbers)))
+
+
+def get_number():
+    valid_input = False
+    while not valid_input:
+        try:
+            the_num = int(input("Number: "))
+            valid_input = True
+        except ValueError:
+            print("Integers please!")
+
+    return the_num
+
+if __name__ == '__main__':
+    main()
