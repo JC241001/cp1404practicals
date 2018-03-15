5e2de63b9a4d8081f3169ca1d120c1b3d92a6db2
jc241001 <gerard.djian@my.jcu.edu.au>
2018-03-16 09:12:02 +1000

    add matching functionality

--- prac_04/intermediate_2.py
+++ prac_04/intermediate_2.py
@@ -1,4 +1,15 @@
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', \
-             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+"""Intermediate exercise with lists of words"""
 
-print(usernames)
+
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', \
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    username = input("UserName: ")
+    if username in usernames:
+        print("Access Granted")
+    else:
+        print("Access Denied")
+
+
+main()
