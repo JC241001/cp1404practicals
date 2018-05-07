bfd89b51c620969eedc8b0ec5b29e0eb3a2fbfe5
JC241001 <gerard.djian@my.jcu.edu.au>
2018-05-08 07:58:22 +1000

    add extract info in all files (Extenstion)

--- prac_09/extension/missing_data.py
+++ prac_09/extension/missing_data.py
@@ -10,47 +10,24 @@ def main():
     """Demo file renaming with the os module."""
     print("Current directory is", os.getcwd())
 
-
-    file_list = os.listdir('.')
-
-    for file in file_list:
-        char_list = []
-        with open(file) as temp_file:
-            for count in range(5):
-                char_list.append(temp_file.readline(2))
-                temp_file.readline()                    # Move cursor to start of next line
-
-        if '.i' not in char_list:
-            print("{} is missing copyright info".format(file))
-
-
-    """
-    # change to desired directory
-    os.chdir('FilesToSortV2')
-
-    # Empty set of directory names
-    dir_name_dict = {}
-
-    # Create directories and dictionary
-    file_list = os.listdir('.')
-    for file in file_list:
-        if not os.path.isfile(file):
+    for dir_name, subdir_list, file_list in os.walk('.'):
+        if dir_name == '.':
             continue
+        os.chdir(dir_name)
+        dir_name = dir_name[2:]
+        for file in file_list:
+            char_list = []
+            with open(file) as temp_file:
+                for count in range(5):
+                    char_list.append(temp_file.readline(2))
+                    temp_file.readline()                    # Move cursor to start of next line
 
-        file_extension = file.split('.')[1]
-        if not file_extension in dir_name_dict.keys():
-            prompt = str('What category would you like to sort {} files into: '.format(file_extension))
-            dir_name = input(prompt)
-            dir_name_dict.update({file_extension:dir_name})
+            if '.i' not in char_list:
+                print("{} from the {} directory is missing copyright information".format(file, dir_name))
 
-            try:
-                os.mkdir(dir_name)
-            except FileExistsError:
-                pass
+        os.chdir('..')
+        print('\n')     # Separate the directories
 
-        shutil.move(file, dir_name_dict.get(file_extension) + '/' + file)
-
-    """
     for dir_name, subdir_list, file_list in os.walk('.'):
         print("In", dir_name)
         print("\tcontains subdirectories:", subdir_list)
