37e5491622267e8537a48317318d6035d59822ce
JC241001 <gerard.djian@my.jcu.edu.au>
2018-05-07 18:25:04 +1000

    add v2

--- prac_09/files_to_sort_v2.py
+++ prac_09/files_to_sort_v2.py
@@ -11,25 +11,30 @@ def main():
     print("Current directory is", os.getcwd())
 
     # change to desired directory
-    os.chdir('FilesToSortV1')
+    os.chdir('FilesToSortV2')
 
     # Empty set of directory names
-    dir_name_set = set()
+    dir_name_dict = {}
 
+    # Create directories and dictionary
     file_list = os.listdir('.')
     for file in file_list:
         if not os.path.isfile(file):
             continue
 
-        dir_name = file.split('.')[1]
-        dir_name_set.add(dir_name)
+        file_extension = file.split('.')[1]
+        if not file_extension in dir_name_dict.keys():
+            prompt = str('What category would you like to sort {} files into: '.format(file_extension))
+            dir_name = input(prompt)
+            dir_name_dict.update({file_extension:dir_name})
 
-        try:
-            os.mkdir(dir_name)
-        except FileExistsError:
-            pass
+            try:
+                os.mkdir(dir_name)
+            except FileExistsError:
+                pass
 
-        shutil.move(file, dir_name + '/' + file)
+        shutil.move(file, dir_name_dict.get(file_extension) + '/' + file)
+
 
     for dir_name, subdir_list, file_list in os.walk('.'):
         print("In", dir_name)
