4153a3384959e36dbca6b833741eaf425d625a4f
JC241001 <gerard.djian@my.jcu.edu.au>
2018-05-21 16:04:53 +1000

    add summary functionality

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -10,12 +10,13 @@ print('Welcome to the Wikipedia interaction extravaganza')
 
 search_phrase = input("Enter search phrase: ")
 while search_phrase != '':
-    handle = wikipedia.summary(search_phrase)
-
-    #print('Suggested title is: '.format(handle.title(search_phrase)))
+    try:
+        print('SUMMARY: {}\n'.format(wikipedia.summary(search_phrase, "html.parser", "markup_type")))
+    except wikipedia.exceptions.DisambiguationError as e:
+        print('SUMMARY (suggested): {}\n'.format(wikipedia.summary(e.options[0])))
+    except wikipedia.exceptions.PageError as f:
+        print("{} does not match any pages\n".format(search_phrase))
     search_phrase = input("Enter search phrase: ")
-
-
 
 
 # my_list = wikipedia.search("Graph Theory")
@@ -35,6 +36,4 @@ while search_phrase != '':
 # print()
 #
 # print(wikipedia.summary('GitHub'))
-
-
 
